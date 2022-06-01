03dad94ef346550a9c970d414b24d11c45445fbd
mattsershaw <masato.sasho@gmail.com>
2022-06-01 23:38:23 +0900

    CRUDのメソッド作成

--- src/app.js
+++ src/app.js
@@ -24,3 +24,53 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('connected mysql');
 });
+
+// todoすべてを取得する
+app.get("/api/todos", (req, res, next) => {
+    const sql = 'select * from todos';
+    connection.query(sql, (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// todo1件を取得する
+app.get("/api/todos/:id", (req, res, next) => {
+  const id = req.params.id;
+  const sql = 'select * from todos where ?';
+  connection.query(sql, {id: id}, (err, results) => {
+      if (err) throw err;
+      res.json(results[0]);
+  });
+});
+
+// todo1件を作成する
+app.post("/api/todos", (req, res, next) => {
+  const todo = req.body;
+  const sql = 'insert into todos set ?';
+  connection.query(sql, todo, (err, result) => {
+      if (err) throw err;
+      res.status(201).json(result.id);
+  });
+});
+
+// todo1件を更新する
+app.put("/api/todos/:id", (req, res, next) => {
+  const id = req.params.id;
+  const todo = req.body;
+  const sql = 'update todos set ? where ?';
+  connection.query(sql, [todo, {id: id}], (err, result) => {
+    if (err) throw err;
+    res.status(200).send();
+  });
+});
+
+// todo1件を削除する
+app.delete("/api/todos/:id", (req, res, next) => {
+  const id = req.params.id; 
+  const sql = 'delete from todos where ?';
+  connection.query(sql, {id: id}, (err, result) => {
+    if (err) throw err;
+    res.status(204).send();
+  });
+});
